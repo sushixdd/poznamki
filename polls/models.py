f098f1672e6365c30500a0f5018325467ccefa00
suchomelj97@gmail.com <suchomelj97@gmail.com>
2021-03-07 16:48:50 +0100

    xd

--- polls/models.py
+++ polls/models.py
@@ -13,3 +13,9 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+
+class Kushovna(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
